07ace76f399b71cec266892965edd0cf4c9969ad
Max <73665502+maxgeor@users.noreply.github.com>
2022-11-10 12:50:29 +0000

    replaced subgrou border highlihgt with chevron

--- components/StyleGroup.jsx
+++ components/StyleGroup.jsx
@@ -13,7 +13,7 @@ export default function StyleGroup({
   const StyleList = ({ children: c }) => (
     <section className={`
       flex flex-col gap-y-5 border-box border-white/[8%]
-      ${isSubgroup ? 'pt-5 pl-6 border-box border-l ' : 'md:pl-6  md:border-l'}
+      ${isSubgroup ? 'mt-5 pl-6 border-box border-l ' : 'md:pl-6  md:border-l'}
     `}>
       {c}
     </section>
@@ -24,24 +24,22 @@ export default function StyleGroup({
       {isSubgroup ? (
         <>
           <button 
-            className='pl-3 p-2 hover:bg-gray-800 rounded-full -mt-2 -ml-3 -mb-2'
+            className='group flex items-center pl-4 p-2 hover:bg-gray-800 rounded-full -mt-2 -ml-4 -mb-2'
             onClick={() => setOpen(!open)}
           >
-            <div className='relative group flex items-center w-full gap-x-6 pl-3.5 border-box border-l border-gray-100 h-3'>
-              <div className="flex items-center gap-x-3">
-                <h3 className='text-xs uppercase text-white tracking-widest whitespace-nowrap'>
-                  {name}
-                </h3>
-                {open ? null : (
-                  <div className='flex gap-x-1'>
-                    {console.log(children)}
-                    {children.slice(0, 3).map(c => <ColorBadge key={c.props.name} color={c.props.color} preview={true} />)}
-                  </div>
-                )}
-              </div>
-              <span className="hidden md:block absolute right-0 -top-0.5">
-                {open ? <ChevronUpIcon className='hidden group-hover:block shrink-0' /> : <ChevronDownIcon className='shrink-0' />}
-              </span>
+            <span className="-ml-[7px]">
+              {open ? <ChevronUpIcon className='shrink-0' /> : <ChevronDownIcon className='shrink-0' />}
+            </span>
+            <div className="flex items-center gap-x-3 w-full pl-2.5 h-3">
+              <h3 className='text-xs uppercase text-white tracking-widest whitespace-nowrap'>
+                {name}
+              </h3>
+              {open ? null : (
+                <div className='flex gap-x-1'>
+                  {console.log(children)}
+                  {children.slice(0, 3).map(c => <ColorBadge key={c.props.name} color={c.props.color} preview={true} />)}
+                </div>
+              )}
             </div>
           </button>
           {open ? <StyleList>{children}</StyleList> : null}
